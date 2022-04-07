d098cb80a3f46f187f5c0d6fc68691228a8bd4e3
fathi129 <nasreen.fathi@gmail.com>
2022-04-07 11:06:56 -0500

    Final code

--- PyPoll.py
+++ PyPoll.py
@@ -27,7 +27,7 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
     # Print each row in the CSV file.
     for row in file_reader:
         total_votes = total_votes+1
@@ -36,13 +36,25 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
-    print(total_votes) 
-    print(candidate_options)
-    print(candidate_votes)
+    # print(total_votes) 
+    # print(candidate_options)
+    # print(candidate_votes)
+with open(file_to_save,'w')  as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"----------------------\n"
+        f"Total Votes:{total_votes:,}\n"
+        f"----------------------\n"
+    )
+    print(election_results,end="")
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes)/float(total_votes) * 100
-        print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+        candidate_result = (f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_result)
+        txt_file.write(candidate_result)
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
@@ -54,7 +66,8 @@ with open(file_to_load) as election_data:
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary)        
+    print(winning_candidate_summary) 
+    txt_file.write(winning_candidate_summary)       
      
 
 

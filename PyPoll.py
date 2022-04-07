4aa16980b9a714fd31dbfdc2709e98d23bc2118e
fathi129 <nasreen.fathi@gmail.com>
2022-04-07 10:36:51 -0500

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -14,7 +14,13 @@ import os
 file_to_load = os.path.join("Resources","election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
-
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 with open(file_to_load) as election_data:
     # To do: read and analyze the data here.
     # Read the file object with the reader function.
@@ -22,6 +28,37 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes = total_votes+1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+    print(total_votes) 
+    print(candidate_options)
+    print(candidate_votes)
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes)/float(total_votes) * 100
+        print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)        
+     
+
+
+                
 
    
 

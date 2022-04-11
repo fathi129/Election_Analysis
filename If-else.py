ac3285f77e718f980c424239ab465f18db5e52cc
fathi129 <nasreen.fathi@gmail.com>
2022-04-10 22:49:20 -0500

    updated file

--- If-else.py
+++
@@ -1,7 +0,0 @@
-temp = int(input("Enter the current temperature"))
-if(temp>=80):
-    print("Turn on the AC")
-    print("The temperature is " +str(temp)+ "F now")
-else:
-    print("open the windows") 
-    print("The temperature is " +str(temp)+ "F now")
